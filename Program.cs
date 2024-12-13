35e44b190a7872e9ff50f7d48f72468650217f85
falau <falau@LENOVO>
2024-12-13 18:38:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Set2Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lungimea secventei de numere: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] v = new int[n];
+            Console.WriteLine("Elementele secventei: ");
+
+            int prev = int.Parse(Console.ReadLine());
+            bool esteCresc = true;
+            bool esteDesc = true;
+
+
+            for(int i = 1; i < n; i++)
+            {
+                int curent = int.Parse(Console.ReadLine());
+                if(curent < prev)
+                {
+                    esteCresc = false;
+                }
+                if(curent > prev)
+                {
+                    esteDesc = false;
+                }
+
+                prev = curent;
+            }
+            
+            if(esteCresc || esteDesc)
+            {
+                Console.WriteLine("Seria este monotona");
+            }
+            else
+            {
+                Console.WriteLine("Nu este monotona");
+            }
+            
+        }
+    }
+}
